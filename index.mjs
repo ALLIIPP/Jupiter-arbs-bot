95d131a24554b1f55cb4660c9d722ea55121a7d4
marcus <>
2022-11-01 16:49:20 -0400

    fixed

--- index.mjs
+++ index.mjs
@@ -27,88 +27,25 @@ import {
   writeFileSync
 } from 'fs';
 
-console.log({ dotenv });
 dotenv.config();
-
-
 //rpc
-const connection = new Connection('');// your rpc
+const connection = new Connection(process.env.RPC_URL);// your rpc
 
 //wallet
 const wallet = (
-  Keypair.fromSecretKey(bs58.decode(''))//your wallet
+  Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY))//your wallet
 );
 
 
 const SOL_MINT = "So11111111111111111111111111111111111111112";
 
-//amount in usd to select quote
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const our_lookuptable = new PublicKey('J9Erb5TwBK8bsgzSisnXhHw3PF4xtnWXyhHc31SCAmJt');
+
 
 
 let somestuff = JSON.parse(readFileSync('somestuff.json').toString())
 
-
-// creates wsol account with 2 SOL
-const createWSolAccount = async () => {
-  const wsolAddress = await Token.getAssociatedTokenAddress(
-    ASSOCIATED_TOKEN_PROGRAM_ID,
-    TOKEN_PROGRAM_ID,
-    new PublicKey(SOL_MINT),
-    wallet.publicKey
-  );
-
-  const wsolAccount = await connection.getAccountInfo(wsolAddress);
-
-  // if no wrapped sol accout, create it 
-
-  const transaction = new Transaction({
-    feePayer: wallet.publicKey,
-  });
-  const instructions = [];
-
-
-  instructions.push(
-    await Token.createAssociatedTokenAccountInstruction(
-      ASSOCIATED_TOKEN_PROGRAM_ID,
-      TOKEN_PROGRAM_ID,
-      new PublicKey(SOL_MINT),
-      wsolAddress,
-      wallet.publicKey,
-      wallet.publicKey
-    )
-  );
-
-  // fund 2 sol to the account
-  instructions.push(
-    SystemProgram.transfer({
-      fromPubkey: wallet.publicKey,
-      toPubkey: wsolAddress,
-      lamports: 2_000_000_000, // 2 sol
-    })
-  );
-
-  instructions.push(
-    // This is not exposed by the types, but indeed it exists
-    Token.createSyncNativeInstruction(TOKEN_PROGRAM_ID, wsolAddress)
-  );
-
-  transaction.add(...instructions);
-  transaction.recentBlockhash = await (
-    await connection.getRecentBlockhash()
-  ).blockhash;
-  transaction.partialSign(wallet.payer);
-  const result = await connection.sendTransaction(transaction, [
-    wallet.payer,
-  ]);
-  console.log({ result });
-
-
-  return wsolAccount;
-};
 
 //get route for swap
 const getCoinQuote = (inputMint, outputMint, amount) =>
@@ -184,8 +121,8 @@ let titles = [
     ["mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So", 'mSOL'],      //mSOL
     ["7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj", 'stSOL'],    //stSOL */
   ["EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", 'USDC'],     // USDC
-  /*  ["Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB", 'USDT'],     // USDT 
-    //  ["So11111111111111111111111111111111111111112", 'WSOL'],      // WSOl
+  ["Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB", 'USDT'],     // USDT 
+  /* 
     ["zwqe1Nd4eiWyCcqdo4FgCq7LYZHdSeGKKudv6RwiAEn", 'SOLPAY'],
     ['HfYFjMKNZygfMC8LsQ8LtpPsPxEJoXJx4M6tqi75Hajo', 'CWAR'],
     ['5jFnsfx36DyGk8uVGrbXnVUMTsBkPXGpx6e69BiGFzko', 'INU'],
@@ -198,233 +135,227 @@ let titles = [
 
 
 //await createWSolAccount()
-await setUp()
+
 //await makeLookup()
 //await what()
 
 
-while (true) {
-
-  for (let quote of quotes) {
-    for (let base of bases) {
-
-      //txn will fail if no quote to buy 
-      if (quote.amount == 0) {
-        continue;
-      }
-
-      let our_initial = Math.floor((quote.amount * .5))
+arb();
+async function arb() {
 
 
-      const usdcToSol = await getCoinQuote(quote.address, base.address, our_initial);
-
-      const solToUsdc = await getCoinQuote(
-        base.address,
-        quote.address,
-        usdcToSol.data[0].outAmount
-      );
-
-      // when outAmount more than initial
-      var returns = (((solToUsdc.data[0].outAmount / our_initial) - 1))
-      console.log('returns : ' + quote.name + '<->' + base.name + '   : ' + returns)
-      // if (returns > .0001) {
-      if (returns) {
-
-        let bread = solToUsdc.data[0].outAmount - our_initial
-        console.log('how much  : ' + bread / LAMPORTS_PER_SOL)
-
-        console.log(quote.name + ":" + base.name)
+  let table = await setUp();
+  const our_lookuptable = new PublicKey(table);
 
 
-        if (!Object.keys(somestuff).includes(quote.name + " <-> " + base.name)) {
-          somestuff[quote.name + " <-> " + base.name] = []
+
+  while (true) {
+
+    for (let quote of quotes) {
+      for (let base of bases) {
+
+        //txn will fail if no quote to buy 
+        if (quote.amount == 0) {
+          continue;
         }
 
-        let instructions = []
-        let signers = []
-        await Promise.all(
-          [usdcToSol.data[0], solToUsdc.data[0]].map(async (route) => {
-            const { setupTransaction, swapTransaction, cleanupTransaction } =
-              await getTransaction(route);
+        let our_initial = Math.floor((quote.amount * .5))
 
-            await Promise.all(
-              [setupTransaction, swapTransaction, cleanupTransaction]
-                .filter(Boolean)
-                .map(async (serializedTransaction) => {
+        const solToUsdc = await getCoinQuote(
+          quote.address,
+          base.address,
+          our_initial
+        );
+
+        const usdcToSol = await getCoinQuote(
+          base.address,
+          quote.address,
+          solToUsdc.data[0].outAmount
+        );
 
 
-                  let transaction = Transaction.from(
-                    Buffer.from(serializedTransaction, "base64")
-                  );
-                  try {
-                    instructions.push(...transaction.instructions)
-                    if (transaction.signers) {
-                      if (transaction.signers.length > 0) {
-                        signers.push(...transaction.signers)
+        var sol_returns = ((usdcToSol.data[0].outAmount / our_initial) - 1)
+        console.log('returns : ' + quote.name + '<->' + base.name + '   : ' + sol_returns)
+
+        if (sol_returns>.0001) { // if returns greater than .01%
+
+          console.log('our_initial : ' + our_initial)
+          let bread = usdcToSol.data[0].outAmount - our_initial
+          console.log('how much  : ' + bread / LAMPORTS_PER_SOL)
+
+          console.log(quote.name + ":" + base.name)
+
+
+          if (!Object.keys(somestuff).includes(quote.name + " <-> " + base.name)) {
+            somestuff[quote.name + " <-> " + base.name] = []
+          }
+
+          let instructions = []
+          let signers = []
+          await Promise.all(
+            [solToUsdc.data[0], usdcToSol.data[0]].map(async (route) => {
+              const { setupTransaction, swapTransaction, cleanupTransaction } =
+                await getTransaction(route);
+
+              await Promise.all(
+                [setupTransaction, swapTransaction, cleanupTransaction]
+                  .filter(Boolean)
+                  .map(async (serializedTransaction) => {
+
+
+                    let transaction = Transaction.from(
+                      Buffer.from(serializedTransaction, "base64")
+                    );
+                    try {
+                      instructions.push(...transaction.instructions)
+                      if (transaction.signers) {
+                        if (transaction.signers.length > 0) {
+                          signers.push(...transaction.signers)
+                        }
                       }
+                    } catch (err) {
+                      console.log(err)
                     }
-                  } catch (err) {
-                    console.log(err)
-                  }
-                })
-            );
-          })
-        );
+                  })
+              );
+            })
+          );
 
 
-        let addresses_1 = []
-        let addresses_2 = []
-        let addresses_3 = []
+          let addresses_1 = []
+          let addresses_2 = []
+          let addresses_3 = []
 
 
 
-        let messageV0 = new TransactionMessage({
-          payerKey: wallet.publicKey,
-          recentBlockhash: await connection.getRecentBlockhash().blockhash,
-          instructions,
-        }).compileToV0Message();
+          let messageV0 = new TransactionMessage({
+            payerKey: wallet.publicKey,
+            recentBlockhash: await connection.getRecentBlockhash().blockhash,
+            instructions,
+          }).compileToV0Message();
 
-        for (let i = 0; i < messageV0.staticAccountKeys.length; i++) {
 
-          if (i < messageV0.staticAccountKeys.length / 3) {
-            if (!somestuff[quote.name + " <-> " + base.name].includes(messageV0.staticAccountKeys[i])) {
-              somestuff[quote.name + " <-> " + base.name].push(messageV0.staticAccountKeys[i])
-              addresses_1.push(messageV0.staticAccountKeys[i])
-              writeFileSync('./somestuff.json', JSON.stringify(somestuff))
+          let bool = false;
+          let unmatched = []
+          for (let element of messageV0.staticAccountKeys) {
+            bool = false;
+            for (let thing of somestuff[quote.name + " <-> " + base.name]) {
+              if (element == thing) {
+                bool = true;
+                break;
+              }
             }
-          } else if (i < messageV0.staticAccountKeys.length / 3 * 2 && (i >= messageV0.staticAccountKeys.length / 3)) {
-            if (!somestuff[quote.name + " <-> " + base.name].includes(messageV0.staticAccountKeys[i])) {
-              somestuff[quote.name + " <-> " + base.name].push(messageV0.staticAccountKeys[i])
-              addresses_2.push(messageV0.staticAccountKeys[i])
-              writeFileSync('./somestuff.json', JSON.stringify(somestuff))
-            }
-
-          } else if (i >= messageV0.staticAccountKeys.length / 3 * 2) {
-            if (!somestuff[quote.name + " <-> " + base.name].includes(messageV0.staticAccountKeys[i])) {
-              somestuff[quote.name + " <-> " + base.name].push(messageV0.staticAccountKeys[i])
-              addresses_3.push(messageV0.staticAccountKeys[i])
-              writeFileSync('./somestuff.json', JSON.stringify(somestuff))
+            if (!bool) {
+              unmatched.push(element);
             }
           }
-        }
+
+          console.log('unmatched length : ' + unmatched.length)
+
+          if (unmatched.length > 15) {
+            for (let i = 0; i < unmatched.length; i++) {
+              if (i < unmatched.length / 3) {
+                addresses_1.push(unmatched[i])
+              } else if (i < unmatched.length / 3 * 2 && (i >= unmatched.length / 3)) {
+                addresses_2.push(unmatched[i])
+              } else if (i >= unmatched.length / 3 * 2) {
+                addresses_3.push(unmatched[i])
+              }
+
+            }
 
 
 
-        const extendInstruction1 = AddressLookupTableProgram.extendLookupTable({
-          payer: wallet.publicKey,
-          authority: wallet.publicKey,
-          lookupTable: our_lookuptable,
-          addresses: addresses_1
+            const extendInstruction1 = AddressLookupTableProgram.extendLookupTable({
+              payer: wallet.publicKey,
+              authority: wallet.publicKey,
+              lookupTable: our_lookuptable,
+              addresses: addresses_1
 
-        });
-        const extendInstruction2 = AddressLookupTableProgram.extendLookupTable({
-          payer: wallet.publicKey,
-          authority: wallet.publicKey,
-          lookupTable: our_lookuptable,
-          addresses: addresses_2
+            });
+            const extendInstruction2 = AddressLookupTableProgram.extendLookupTable({
+              payer: wallet.publicKey,
+              authority: wallet.publicKey,
+              lookupTable: our_lookuptable,
+              addresses: addresses_2
 
-        });
-        const extendInstruction3 = AddressLookupTableProgram.extendLookupTable({
-          payer: wallet.publicKey,
-          authority: wallet.publicKey,
-          lookupTable: our_lookuptable,
-          addresses: addresses_3
+            });
+            const extendInstruction3 = AddressLookupTableProgram.extendLookupTable({
+              payer: wallet.publicKey,
+              authority: wallet.publicKey,
+              lookupTable: our_lookuptable,
+              addresses: addresses_3
 
-        });
+            });
 
-        console.log('lut 1 : ' + addresses_1.length)
-        console.log('lut 2 : ' + addresses_2.length)
-        console.log('lut 3 : ' + addresses_3.length)
+            console.log('lut 1 : ' + addresses_1.length)
+            console.log('lut 2 : ' + addresses_2.length)
+            console.log('lut 3 : ' + addresses_3.length)
 
-        let ix2 = [extendInstruction1, extendInstruction2, extendInstruction3]
+            let ix2 = [
+              [extendInstruction1, addresses_1],
+              [extendInstruction2, addresses_2],
+              [extendInstruction3, addresses_3]
+            ]
 
-        console.log('all good till here ')
+            console.log('all good till here ')
 
-      /*  for (let element of ix2) {
-          let tx = new Transaction()
-          tx.add(element)
+            for (let element of ix2) {
+              let tx = new Transaction()
+              tx.add(element[0])
+
+              let blockhash = await connection
+                .getLatestBlockhash()
+                .then((res) => res.blockhash);
+              tx.recentBlockhash = blockhash
+              tx.sign(wallet)
+              try {
+                await sendAndConfirmTransaction(connection, tx, [wallet], { skipPreflight: true })
+                console.log('sent one look up extension txn...')
+                //TODO write file here
+                somestuff[quote.name + " <-> " + base.name].push(...element[1])
+                writeFileSync('./somestuff.json', JSON.stringify(somestuff))
+              } catch (err) {
+                console.log('!ttt first failed : ' + err)
+              }
+            }
+          } else {
+            // continually get recent blockhash then pull it here
+            let blockhash = await connection
+              .getLatestBlockhash()
+              .then((res) => res.blockhash);
+
+            const lookupTableAccount = await connection
+              .getAddressLookupTable(our_lookuptable)
+              .then((res) => res.value);
+
+            let messageV00 = new TransactionMessage({
+              payerKey: wallet.publicKey,
+              recentBlockhash: blockhash,
+              instructions,
+            }).compileToV0Message([lookupTableAccount]);
+
+            let transaction = new VersionedTransaction(messageV00);
+
+            transaction.sign([wallet])
 
 
-          let blockhash = await connection
-            .getLatestBlockhash()
-            .then((res) => res.blockhash);
-          tx.recentBlockhash = blockhash
-          tx.sign(wallet)
-          try {
-            await sendAndConfirmTransaction(connection, tx, [wallet], { skipPreflight: true })
-            console.log('sent one look up extension txn...')
-          } catch (err) {
-            console.log('!ttt first failed : ' + e)
+            try {
+              let tx_id = await sendAndConfirmTransaction(connection, transaction)
+              console.log('swapped ' + tx_id)
+            } catch (err) {
+              console.log("hererrererrrr444444" + err)
+            }
+            break;
+
           }
 
         }
-        */
-
-        let lookupTableAccount = await connection
-          .getAddressLookupTable(our_lookuptable)
-          .then((res) => res.value);
-
-        let blockhash = await connection
-          .getLatestBlockhash()
-          .then((res) => res.blockhash);
-
-        let messageV00 = new TransactionMessage({
-          payerKey: wallet.publicKey,
-          recentBlockhash: blockhash,
-          instructions,
-        }).compileToV0Message([lookupTableAccount]);
-
-        let transaction = new VersionedTransaction(messageV00);
-
-        transaction.sign([wallet])
-
-        try {
-          let tx_id = await sendAndConfirmTransaction(connection, transaction)
-          console.log('swapped '+tx_id)
-        } catch (err) {
-          console.log("hererrererrrr444444" + err)
-        }
-        break;
-
-
-        /*
-        // get transaction object from serialized transaction
-        const transaction = Transaction.from(
-          Buffer.from(serializedTransaction, "base64")
-        );
-        // perform the swap
-        // Transaction might failed or dropped
-        const txid = await connection.sendTransaction(
-          transaction,
-          [wallet.payer],
-          {
-            skipPreflight: true,
-          }
-        );
-        //use route to figure out whats in wallet 
-        try {
-          await getConfirmTransaction(txid);
-
-          console.log(`Success: https://solscan.io/tx/${txid}`);
-        } catch (e) {
-          console.log(`Failed: https://solscan.io/tx/${txid}`);
-          // if one txn fails, reasses
-          // with lookuptables we wont have to do this , we can fit everything in one txn
-          switcharoo(base_initial, quote_initial, base, quote)
-
-
-        }
-            */
-
       }
     }
   }
 }
 
-
- 
-
- 
 
 async function setUp() {
 
@@ -432,15 +363,26 @@ async function setUp() {
 
   // TODO check if setup.json exits 
   // calling setup will fill 'bases' and 'quote' with
-/*
-{
-  address:    //for route
-  name:       //for console.log 
-  amount:     //for calculating buys
-  decimal:    //for calculating buys
+  /*
+  {
+    address:    //for route
+    name:       //for console.log 
+    amount:     //for calculating buys
+    decimal:    //for calculating buys
+  
+  }
+  */
 
-}
-*/
+  let lutacc = JSON.parse(readFileSync('lut.json'));
+  let addy;
+  if (!lutacc.address) {
+    addy = await makeLookup()
+    while (addy == '') {
+      addy = await makeLookup()
+    }
+    lutacc.address = addy
+    writeFileSync('lut.json', JSON.stringify(lutacc))
+  }
 
   let bingus = await connection.getParsedTokenAccountsByOwner(wallet.publicKey, { programId: TOKEN_PROGRAM_ID })
 
@@ -475,8 +417,8 @@ async function setUp() {
 
   }
 
-  console.log('quotes : ' + JSON.stringify(quotes))
 
+  return lutacc.address;
 }
 
 
@@ -563,8 +505,6 @@ async function makeLookup() {
       recentSlot: slot,
     });
 
-
-
   let tx2 = new Transaction()
   tx2.add(lookupTableInst)
 
@@ -574,28 +514,15 @@ async function makeLookup() {
   tx2.recentBlockhash = blockhash
   tx2.sign(wallet)
 
-  console.log('addy : ' + lookupTableAddress)
+  console.log('lut address : ' + lookupTableAddress)
   try {
     await sendAndConfirmTransaction(connection, tx2, [wallet], { skipPreflight: true })
+
+    return lookupTableAddress;
   } catch (err) {
     console.log('failed  : ' + e)
+    return ''
   }
-
 }
 
-
-async function what() {
-  let ttt = await connection
-    .getAddressLookupTable(our_lookuptable)
-    .then((res) => {
-      console.log('what : ' + res)
-      return res.value
-    })
-    .catch(e => {
-      console.log(e)
-    })
-
-
-  console.log(ttt)
-}
 
